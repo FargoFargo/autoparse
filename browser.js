9403c76dae7906e7d9707844fabc764702ce7617
rback8 <yegorovcrypta@gmail.com>
2022-01-04 19:41:57 +0300

    Timeout 0

--- browser.js
+++ browser.js
@@ -6,6 +6,7 @@ async function startBrowser(){
         console.log("Opening the browser......");
         browser = await puppeteer.launch({
             headless: false,
+            timeout: 0,
             args: ["--disable-setuid-sandbox", '--no-sandbox'],
             'ignoreHTTPSErrors': true
         });
